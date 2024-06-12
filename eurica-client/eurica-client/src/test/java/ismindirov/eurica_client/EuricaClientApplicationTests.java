5fc30d1ab932a16d138236a2fddb235f3f950913
ismindirov <ismindirov@gmail.com>
2024-06-12 13:05:16 +0200

    add

---
+++ eurica-client/eurica-client/src/test/java/ismindirov/eurica_client/EuricaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ismindirov.eurica_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuricaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eurika-server/eurica-server/src/test/java/ismindirov/eurica_server/EuricaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ismindirov.eurica_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuricaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
